dcd08120a24b8d7fa9cea112ad81a51844a87c11
Oliver Krieger <oliver.krieger76@gmail.com>
2024-11-02 12:34:58 +0000

    add logging values

--- backend/fetch_data/app.py
+++ backend/fetch_data/app.py
@@ -1,10 +1,12 @@
 from flask import Flask, jsonify, request
 import requests
+import logging
 
 app = Flask(__name__)
 
 @app.route("/fetch_data", methods=["GET"])
 def fetch_data():
+    logging.info('Fetching data from URL.')
     url = request.args.get("url") # get url from path
     
     if not url:
@@ -15,6 +17,7 @@ def fetch_data():
         response.raise_for_status()
         data = response.json() # convert to json
     except requests.exceptions.RequestException as e:
+        logging.error(f"Error fetching data: {str(e)}")
         return jsonify({"error": str(e)}), 500 # if error here, return 500 error
 
     return jsonify(data) # otherwise return data from request

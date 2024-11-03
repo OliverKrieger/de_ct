6be32468726ef6fd032e7f8de72416f48180c6eb
Oliver Krieger <oliver.krieger76@gmail.com>
2024-11-03 16:52:03 +0000

    add tailwind support

--- frontend/src/components/data_requests/RequestDatasets.vue
+++ frontend/src/components/data_requests/RequestDatasets.vue
@@ -1,17 +1,19 @@
 <template>
     <div class="data-request-component">
-        <h1>Datasets</h1>
-        <div v-if="datasets" v-for="(dataset, index) in datasets.Datasets.Dataset" :key="index">
-            <div class="dataset-card">
-                <h2>{{ dataset.DatasetName }}</h2>
-                <p><strong>Code:</strong> {{ dataset.DatasetCode }}</p>
-                <p><strong>Description:</strong> {{ dataset.DatasetDescription }}</p>
-                <p><strong>Contact:</strong> {{ dataset.Contact }}</p>
-                <p><strong>Email:</strong> {{ dataset.Email }}</p>
-                <p><strong>Last Updated:</strong> {{ dataset.DateUpdate }}</p>
-                <p><strong>File Size:</strong> {{ dataset.FileSize }}</p>
-                <p><strong>File Type:</strong> {{ dataset.FileType }}</p>
-                <p><strong>File Location:</strong> {{ dataset.FileLocation }}</p>
+        <h1 class="text-xl">Datasets</h1>
+        <div v-if="datasets" class="flex flex-wrap">
+            <div class="w-1/4 p-2 hover:bg-rose-900 transition-all duration-300 cursor-pointer" v-for="(dataset, index) in datasets.Datasets.Dataset" :key="index">
+                <div class="dataset-card">
+                    <h2 class="text-l">{{ dataset.DatasetName }}</h2>
+                    <p><strong>Code:</strong> <span :class="dataset.DatasetCode=='GCE' ? 'text-rose-500' : ''"> {{ dataset.DatasetCode }} </span></p>
+                    <!-- <p><strong>Description:</strong> {{ dataset.DatasetDescription }}</p>
+                    <p><strong>Contact:</strong> {{ dataset.Contact }}</p>
+                    <p><strong>Email:</strong> {{ dataset.Email }}</p>
+                    <p><strong>Last Updated:</strong> {{ dataset.DateUpdate }}</p>
+                    <p><strong>File Size:</strong> {{ dataset.FileSize }}</p>
+                    <p><strong>File Type:</strong> {{ dataset.FileType }}</p>
+                    <p><strong>File Location:</strong> {{ dataset.FileLocation }}</p> -->
+                </div>
             </div>
         </div>
         <div class="loader-container" v-else>

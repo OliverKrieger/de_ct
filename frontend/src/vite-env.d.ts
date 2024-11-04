26571a238499a512c4bf32c9a2e267b67a7ea44f
Oliver Krieger <oliver.krieger76@gmail.com>
2024-11-04 01:35:24 +0000

    push env file

---
+++ frontend/src/vite-env.d.ts
@@ -0,0 +1,40 @@
+/// <reference types="vite/client" />
+
+declare var process: {
+    env: {
+        VITE_API_URL: string; // Add other environment variables as needed
+    };
+};
+
+declare module '*.vue' {
+    import { DefineComponent } from 'vue';
+    const component: DefineComponent<{}, {}, any>;
+    export default component;
+}
+
+declare module 'vue-chartjs' {
+    import { DefineComponent } from 'vue';
+    import { ChartOptions } from 'chart.js';
+
+    export const Bar: DefineComponent<any, any, any>;
+    export const Line: DefineComponent<any, any, any>;
+    export const Pie: DefineComponent<any, any, any>;
+    export const Doughnut: DefineComponent<any, any, any>;
+    export const PolarArea: DefineComponent<any, any, any>;
+    export const Radar: DefineComponent<any, any, any>;
+    export const Bubble: DefineComponent<any, any, any>;
+    export const Scatter: DefineComponent<any, any, any>;
+
+    export interface ChartProps {
+        chartData: any; // Adjust the type as needed
+        chartOptions?: ChartOptions; // Use ChartOptions for options
+    }
+}
+
+// interface ImportMetaEnv {
+//     readonly VITE_API_URL: string; // Add other environment variables here
+// }
+
+// interface ImportMeta {
+//     readonly env: ImportMetaEnv;
+// }
